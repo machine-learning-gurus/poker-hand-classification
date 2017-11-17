0102c13140ab07906a03f21d136fd291d98212cd
lucas-tata <lucas_tata@student.uml.edu>
2017-11-16 22:39:35 -0500

    Means and Covariances
    Trying to figure out what to do with these values, but this is a start

--- main.m
+++ main.m
@@ -37,3 +37,32 @@ for c = 1:size(X)
             c9 = [c9;X(c, :)];
     end;
 end;
+
+%compute the covariances and means (we need to do that for something im pretty sure)
+
+c0mean = mean(c0);
+c1mean = mean(c1);
+c2mean = mean(c2);
+c3mean = mean(c3);
+c4mean = mean(c4);
+c5mean = mean(c5);
+c6mean = mean(c6);
+c7mean = mean(c7);
+c8mean = mean(c8);
+c9mean = mean(c9);
+
+means = [c0mean; c1mean; c2mean; c3mean; c4mean; c5mean; c6mean; c7mean; c8mean; c9mean];
+
+meanOfMeans = mean(means);
+
+c0cov = cov(c0);
+c1cov = cov(c1);
+c2cov = cov(c2);
+c3cov = cov(c3);
+c4cov = cov(c4);
+c5cov = cov(c5);
+c6cov = cov(c6);
+c7cov = cov(c7);
+c8cov = cov(c8);
+c9cov = cov(c9);
+
