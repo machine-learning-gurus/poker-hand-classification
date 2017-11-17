ce121aee65c07540b8fb8afe491df5877576dce8
lucas-tata <lucas_tata@student.uml.edu>
2017-11-16 22:20:16 -0500

    Category Splitting
    Added loop to put data into their respective classes

--- main.m
+++ main.m
@@ -1,3 +1,39 @@
 data = dlmread("data/poker-hand-training-true.data",",");
-y = data(:,1);
-X = data(:,2:end);
+y = data(:,end);
+X = data(:,1:end-1);
+
+c0 = [];
+c1 = [];
+c2 = [];
+c3 = [];
+c4 = [];
+c5 = [];
+c6 = [];
+c7 = [];
+c8 = [];
+c9 = [];
+
+for c = 1:size(X)
+    switch y(c)
+        case 0
+            c0 = [c0;X(c, :)];
+        case 1
+            c1 = [c1;X(c, :)];
+        case 2
+            c2 = [c2;X(c, :)];
+        case 3
+            c3 = [c3;X(c, :)];
+        case 4
+            c4 = [c4;X(c, :)];
+        case 5
+            c5 = [c5;X(c, :)];
+        case 6
+            c6 = [c6;X(c, :)];
+        case 7
+            c7 = [c7;X(c, :)];
+        case 8
+            c8 = [c8;X(c, :)];
+        case 9
+            c9 = [c9;X(c, :)];
+    end;
+end;
